b96b59713d59d8c0ee6851648b94cd49088dcfa7
Daichi Kawakami <kbc16a05@stu.kawahara.ac.jp>
2018-09-12 15:27:42 +0900

    [create]initial commit

---
+++ src/app/block/memo/memo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memo',
+  templateUrl: './memo.component.html',
+  styleUrls: ['./memo.component.scss']
+})
+export class MemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
