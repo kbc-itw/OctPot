071ee5277c07f0b212f5c1281a24125b23a80623
kbc16a08-sakamoto <kbc16a08@stu.kawahara.ac.jp>
2018-11-30 10:01:45 +0900

    [change]トップページのデザインの変更と修正
    トップページのデザインに関係するhtmlとcssのapp.module.tsのコードの変更と修正をしました。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { MY_ROUTES } from './app.routing';
+import {MY_ROUTES} from './app.routing';
 
-import { AppComponent } from './app.component';
-import { CharacterCreateComponent } from './component/character-create/character-create.component';
-import { CharacterManagementComponent } from './component/character-management/character-management.component';
-import { ChatRoomComponent } from './component/chat-room/chat-room.component';
-import { TopPageComponent } from './component/top-page/top-page.component';
-import { ChatRoomCreateComponent } from './component/chat-room-create/chat-room-create.component';
-import { DiceComponent } from './component/dice/dice.component';
-import { MemoComponent } from './component/memo/memo.component';
+import {AppComponent} from './app.component';
+import {CharacterCreateComponent} from './component/character-create/character-create.component';
+import {CharacterManagementComponent} from './component/character-management/character-management.component';
+import {ChatRoomComponent} from './component/chat-room/chat-room.component';
+import {TopPageComponent} from './component/top-page/top-page.component';
+import {ChatRoomCreateComponent} from './component/chat-room-create/chat-room-create.component';
+import {DiceComponent} from './component/dice/dice.component';
+import {MemoComponent} from './component/memo/memo.component';
 
-import { CharacterCreateService } from './Service/character-create.service';
-import { CharacterManagementService } from './Service/character-management.service';
-import { ChatRoomService } from './Service/chat-room-service';
-import { ChatRoomCreateService } from './Service/chat-room-create.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {CharacterCreateService} from './Service/character-create.service';
+import {CharacterManagementService} from './Service/character-management.service';
+import {ChatRoomService} from './Service/chat-room-service';
+import {ChatRoomCreateService} from './Service/chat-room-create.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatTabsModule} from '@angular/material';
 
 
 @NgModule({
@@ -28,9 +28,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     DiceComponent, MemoComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule, MY_ROUTES, BrowserAnimationsModule
+    BrowserModule, FormsModule, HttpClientModule, MY_ROUTES, BrowserAnimationsModule, MatButtonModule, MatTabsModule, MatCardModule
   ],
-  providers: [ CharacterCreateService, CharacterManagementService, ChatRoomService, ChatRoomCreateService],
+  providers: [CharacterCreateService, CharacterManagementService, ChatRoomService, ChatRoomCreateService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
