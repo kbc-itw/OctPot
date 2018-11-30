a66d758502605d8901ccbf57328c015ebaaaa0dd
kbc16a08-sakamoto <kbc16a08@stu.kawahara.ac.jp>
2018-11-30 10:17:22 +0900

    [add]メモコンテンツのルート設定の追加
    app.routing.tsにメモコンテンツまでルートを追加しました。

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,20 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { TopPageComponent } from './component/top-page/top-page.component';
-import { CharacterCreateComponent } from './component/character-create/character-create.component';
-import { CharacterManagementComponent } from './component/character-management/character-management.component';
-import { ChatRoomComponent } from './component/chat-room/chat-room.component';
-import { ChatRoomCreateComponent } from './component/chat-room-create/chat-room-create.component';
+import {TopPageComponent} from './component/top-page/top-page.component';
+import {CharacterCreateComponent} from './component/character-create/character-create.component';
+import {CharacterManagementComponent} from './component/character-management/character-management.component';
+import {ChatRoomComponent} from './component/chat-room/chat-room.component';
+import {ChatRoomCreateComponent} from './component/chat-room-create/chat-room-create.component';
+import {MemoComponent} from './component/memo/memo.component';
 
 const myRoutes = [
-  { path: '', component: TopPageComponent },
-  { path: 'create', component: CharacterCreateComponent },
-  { path: 'management', component: CharacterManagementComponent },
-  { path: 'chat-room', component: ChatRoomComponent },
-  { path: 'create-chat-room', component: ChatRoomCreateComponent },
+  {path: '', component: TopPageComponent},
+  {path: 'create', component: CharacterCreateComponent},
+  {path: 'management', component: CharacterManagementComponent},
+  {path: 'chat-room', component: ChatRoomComponent},
+  {path: 'create-chat-room', component: ChatRoomCreateComponent},
+  {path: 'memo', component: MemoComponent},
   // { path: '**', component: ErrorComponent} 予想外のパスにつないだ時のコンポーネント
 ];
 
