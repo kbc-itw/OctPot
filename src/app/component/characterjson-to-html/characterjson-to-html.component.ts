1e7f726b1c565c3ba20bb3633526bf622dff0e91
Hajime <kbc16a15@stu.kawahara.ac.jp>
2018-12-18 15:49:36 +0900

    [add]characterJSONを指定して読み込みたかった

--- src/app/component/characterjson-to-html/characterjson-to-html.component.ts
+++ src/app/component/characterjson-to-html/characterjson-to-html.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterjsonToHtmlComponent implements OnInit {
 
+  private jsonman;
+
   private type = 'SUZUKI';
   private race = 'ばいく';
   private job = '販売業';
@@ -16,4 +18,23 @@ export class CharacterjsonToHtmlComponent implements OnInit {
   ngOnInit() {
   }
 
+  getJson(list: any) {
+    if (list <= 0) { return; } // 何も指定されていなければ何もしない
+
+    var str: any;
+    console.log(list);
+    this.jsonman = list[0];
+    console.log(this.jsonman);
+
+    let reader = new FileReader();
+
+    // reader.resultの値をstrに入れたいけどできないでござる
+    reader.onload = function() {
+      console.log(reader.result);
+      let res = reader.result;
+      str = res.toString;
+    };
+    reader.readAsText(this.jsonman);
+    console.log(str);
+  }
 }
