914542d891aa8c260bbfa4916eb533ccb6a2a525
Ishikawa <kbc16a01@stu.kawahara.ac.jp>
2019-01-15 12:28:20 +0900

    [add]見た目追加

--- src/app/Service/top-room-service.ts
+++ src/app/Service/top-room-service.ts
@@ -2,4 +2,42 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class TopRoomService {}
+export class TopRoomService {
+  constructor () {}
+  click(word) {
+    switch (word) {
+      case 'file':
+        console.log('file');
+        break;
+      case 'view':
+        console.log('view');
+        break;
+      case 'koma':
+        console.log('koma');
+        break;
+      case 'map':
+        console.log('map');
+        break;
+      case 'config':
+        console.log('config');
+        break;
+      case 'exit':
+        console.log('exit');
+        break;
+      default:
+        this.error_log('click', word);
+    }
+    console.log('file_click');
+  }
+
+  error_log(key, word = '') {
+    console.log('error_log');
+    switch (key) {
+      case 'click':
+        console.log('click is error');
+        console.log('想定しないwordです。');
+      default:
+        console.log('想定しないerror');
+    }
+  }
+}
--- src/app/component/top-room/top-room.component.ts
+++ src/app/component/top-room/top-room.component.ts
@@ -3,11 +3,16 @@ import { TopRoomService } from '../../Service/top-room-service';
 
 @Component({
   selector: 'app-top-room',
-  templateUrl: './top-room.component.html'
+  templateUrl: './top-room.component.html',
+  styleUrls: ['./top-room.component.css']
 })
 export class TopRoomComponent implements OnInit {
+  menu;
   constructor(private top: TopRoomService) { }
   ngOnInit() {
     console.log('top-room-component');
+  }
+  click(word) {
+    this.top.click(word);
   }
 }
