39d5b145d357a77d16e40dd1c6a37ecb7e572a10
Ishikawa <kbc16a01@stu.kawahara.ac.jp>
2019-01-16 10:23:56 +0900

    [add]サーバと通信するうえで必要な情報を取得する機能

--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -11,6 +11,8 @@ export class MenuComponent implements OnInit {
   val;
   c_pass: boolean = false;
   pass;
+  name;
+  io;
   constructor(private menu: MenuService, private chatroom: ChatRoomCreateComponent) { }
   /*
   必要な情報
@@ -20,6 +22,10 @@ export class MenuComponent implements OnInit {
    */
   ngOnInit() {
     console.log('memo-component');
+    var params = this.chatroom.get_params();
+    console.log(params[0]);
+    this.pass = params[0].pass;
+    this.name = params[0].name;
   }
   click(word) {
     this.menu.click(word);
@@ -27,11 +33,10 @@ export class MenuComponent implements OnInit {
   change_pass() {
     console.log('change_pass');
     this.c_pass = false;
-  }
-  get_params() {
-    console.log('get_params');
     var params = this.chatroom.get_params();
-    console.log(params);
+    console.log(params[0]);
+    this.io = params[0].io;
+    this.io.emit('change_pass', this.pass);
   }
   leave() {
     this.chatroom.leave();
