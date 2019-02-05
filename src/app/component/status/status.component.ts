b6fa516e162575e6fa3210cb10188037b86570a0
Hajime <kbc16a15@stu.kawahara.ac.jp>
2019-02-05 12:28:03 +0900

    [add]キャラの現在のステータスを操作できるようになりました

--- src/app/component/status/status.component.ts
+++ src/app/component/status/status.component.ts
@@ -12,7 +12,9 @@ import { CharacterDataListService } from '../../Service/character-data-list.serv
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.css']
 })
+
 export class StatusComponent implements OnInit {
+  private charaStatusList: CharaStatus[] = []; // 使用するキャラの現在のステータスが入ってる配列
 
   private charaList: Chara[] = [];  // 使用するキャラが入っている配列
   public subscription: Subscription;
@@ -25,11 +27,45 @@ export class StatusComponent implements OnInit {
       data => {
         let chachacha = Convert.toChara(data);
         this.charaList.push(chachacha);
+        this.charaStatusList.push(new CharaStatus(
+            chachacha.Setting.character.name,
+            chachacha.Status.fluctuationStatus.health,
+            chachacha.Status.fluctuationStatus.san,
+            chachacha.Status.fluctuationStatus.mp));
       }
     );
+  }
+
+  // +-ボタンが押されたら対応するステータスを+-する
+  changeStatus(status: string, point: number, index: number) {
+    switch (status) {
+      case ('hp'):
+        this.charaStatusList[index].hp += point;
+        break;
+      case ('mp'):
+        this.charaStatusList[index].mp += point;
+        break;
+      case ('san'):
+        this.charaStatusList[index].san += point;
+        break;
+    }
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+}
 
+// 使用するキャラの現在のステータスを記録しておくクラス
+export class CharaStatus {
+  name: string = '';
+  hp: number = 0;
+  san: number = 0;
+  mp: number = 0;
+
+  constructor(name: string, hp: number, san: number, mp: number) {
+    this.name = name;
+    this.hp = hp;
+    this.san = san;
+    this.mp = mp;
+  }
 }
