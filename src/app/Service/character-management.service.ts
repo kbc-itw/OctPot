ac3e5c4d0de2fe45c6be0505d64461417b5ae48a
Daichi Kawakami <kbc16a05@stu.kawahara.ac.jp>
2019-02-15 13:49:36 +0900

    [create] localStorage用のサービス

--- src/app/Service/character-management.service.ts
+++ src/app/Service/character-management.service.ts
@@ -6,4 +6,24 @@ export class CharacterManagementService {
     var reader = new FileReader();
     reader.readAsDataURL(new Blob());
   }
+
+  getItem( key ) {
+    return  JSON.parse(localStorage.getItem(key));
+  }
+
+  setItem(key , value) {
+    let ls = [];
+    ls = JSON.parse(localStorage.getItem(key));
+    ls.push( value );
+    localStorage.setItem( key, JSON.stringify(ls));
+
+  }
+
+  delete( key , index ) {
+    let ls = JSON.parse(localStorage.getItem(key));
+    ls.splice( index , 1);
+    localStorage.setItem( key, JSON.stringify(ls));
+
+  }
+
 }
