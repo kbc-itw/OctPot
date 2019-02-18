e4d06941beeae94f03591c8fc2f200033605bc6f
kbc16a02 <kbc16a02@stu.kawahara.ac.jp>
2019-02-18 20:35:46 +0900

    [add]技能表示エリア追加

---
+++ src/app/component/chara-ability/chara-ability.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chara-ability',
+  templateUrl: './chara-ability.component.html',
+  styleUrls: ['./chara-ability.component.css']
+})
+export class CharaAbilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
